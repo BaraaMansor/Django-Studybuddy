831fb0ef9586cdc166681acfb276ade0ce4ba7e5
Baraa <baraadev0@gmail.com>
2024-02-16 09:31:53 +0300

    First Version

---
+++ base/urls.py
@@ -0,0 +1,23 @@
+from django.urls import path
+from base import views
+
+urlpatterns = [
+    path('login/', views.login_page, name="login"),
+    path('register/', views.register, name="register"),
+    path('logout/', views.logout_user, name="logout"),
+    
+    path('', views.home, name="home"),
+    path('room/<str:pk>/', views.room, name="room"),
+    path('profile/<str:pk>/', views.user_profile, name="user_profile"),
+    
+    
+    path('create-room/', views.create_room, name="create_room"),
+    path('update-room/<str:pk>', views.update_room, name="update_room"),
+    path('delete-room/<str:pk>', views.delete_room, name="delete_room"),
+    path('delete-message/<str:pk>', views.delete_message, name="delete_message"),
+    path('update-user/', views.update_user, name="update_user"),
+    
+    # mobile links
+    path('topics/', views.topics, name="topics"),
+    path('activities/', views.activities, name="activities"),
+]
